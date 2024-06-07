6fb167eabbb1cfbd5c6c99697db22c71a01acea0
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-06-07 17:12:30 +0430

    find the title length ;

--- Day-01/main.py
+++ Day-01/main.py
@@ -5,3 +5,17 @@ todo_3 = input(user_prompt)
 
 todo_list = [todo_1, todo_2, todo_3]
 print(todo_list)
+
+# text = input("Enter the Title :")
+#
+# print('Your title length is ',len(text))
+
+
+def title_length(text):
+    result = len(text)
+    return result
+
+
+text = input("Enter Your Text :")
+length = title_length(text)
+print(length)
