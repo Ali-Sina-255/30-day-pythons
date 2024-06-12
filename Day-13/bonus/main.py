32181bcb7ab05c55b0ae44b8c414f1bb466f83ca
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-06-12 17:40:31 +0430

    day 15

--- Day-13/bonus/main.py
+++ Day-13/bonus/main.py
@@ -2,13 +2,26 @@ print('In this programmer you can calculate the feed and inches in maters ')
 print("just provide feed and inches in one line . ex: 2 feed 4 inches")
 feed_inches = input("Enter Feed and Inches :")
 
+"""Decoupling in this function we user Decoupling to access the local Variables in to the goloble varible """
 
-def convert(feed_inches_par):
+
+def parse(feed_inches_par):
     parts = feed_inches_par.split(" ")
     feed = float(parts[0])
     inches = float(parts[1])
+    return {"feed": feed, "inches": inches}
+
+
+def convert(feed, inches):
     meters = feed * 0.3048 + inches * 0.0254
-    return f"{feed} feed {inches} inches is equal to {meters} meters ."
+    return meters
 
 
-print(convert(feed_inches))
+def helper(func, funcs):
+    return f'{func} and {funcs} is called in this time.'
+
+
+parsed = parse(feed_inches)
+
+result = convert(parsed['feed'], parsed['inches'])
+print(f"{parsed['feed']} feed and {parsed['inches']} is equal to {result} meters .")
